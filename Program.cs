1e09347fa02a088c0f5a0fc87052f610f72af27c
Dell T3600 <Dell T3600@DESKTOP-O3ATIL5>
2021-11-07 10:25:24 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+
+namespace StopWatchApplication
+{
+    public class StopWatch
+    {
+        public DateTime start { get; set; }
+        public DateTime stop { get; set; }
+        public bool started = false;
+
+
+        public void startfun(DateTime sf)
+        {
+            if (!started)
+            {
+                start = sf;
+                started = true;
+            }
+        }
+        public void stopfun(DateTime pt)
+        {
+            if (started)
+            {
+                stop = pt;
+                started = false;
+            }
+        }
+        public TimeSpan getinterval()
+        {
+            var duration = start - stop;
+            return duration;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StopWatch stopWatch = new StopWatch();
+            for (int i = 0; i < 2; i++)
+            {
+
+                Thread.Sleep(1000);
+
+            }
+            stopWatch.startfun(DateTime.Now.AddDays(1));
+            stopWatch.stopfun(DateTime.Now.AddYears(-1));
+            Console.WriteLine( stopWatch.getinterval());
+           
+        }
+    }
+}
